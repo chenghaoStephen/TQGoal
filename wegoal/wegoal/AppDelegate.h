b0ca2a813f697b710644111e2b06c1dd9f5c42a8
joker <wch15041168573@163.com>
2016-12-21 17:05:34 +0800

    third dev commit

--- wegoal/wegoal/AppDelegate.h
+++ wegoal/wegoal/AppDelegate.h
@@ -11,8 +11,8 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
+//属性
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) TQTabBarController *tabBarController;
 
 @end
-
